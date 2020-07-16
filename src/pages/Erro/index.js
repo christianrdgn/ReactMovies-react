9278a566978908411e0ba376db648a6f3b8c5ad3
christianrdgn <christian.rdgn1@gmail.com>
2020-07-16 00:43:01 -0300

    Aplicação finalizada

---
+++ src/pages/Erro/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Erro extends Component {
+    render(){
+        return(
+            <div>
+                <h1><em>Página não encontrada!</em></h1>
+            </div>
+        );
+    }
+}
+
+export default Erro;
