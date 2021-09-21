6eeec8f44b52ef0befe4642deb802b53b4a541cb
christianrdgn <christian.rdgn1@gmail.com>
2021-09-21 12:54:21 -0300

    link adicionado na not-found

--- src/pages/Erro/index.js
+++ src/pages/Erro/index.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import './erro.css';
 
 class Erro extends Component {
     render(){
         return(
             <div>
                 <h1><em>Página não encontrada!</em></h1>
+                <Link to="/">Clique aqui e veja todos os filmes!</Link>
             </div>
         );
     }
